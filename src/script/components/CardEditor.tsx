3176ff02d65700bfb1ac47ac1e8a0c8ba3ce03c0
nabby <nabbydude@gmail.com>
2023-06-02 00:32:47 -0400

    improved visual for no card selected

--- src/script/components/CardEditor.tsx
+++ src/script/components/CardEditor.tsx
@@ -15,7 +15,7 @@ export function CardEditor(props: CardEditorProps) {
 	const { card_id } = props;
 	const { doc, v } = useDocumentWithV();
 	const [card, path] = useMemo(() => first_matching_entry<Card>(doc, { type: "Card", id: card_id }) ?? [undefined, undefined], [doc, v, card_id]);
-	if (!card) return (<div>No card focused</div>);
+	if (!card) return (<div className="card_editor empty">No card focused</div>);
 
 	const frame_field = first_matching_element(card, { type: "Field", name: "frame" });
 	const frame_value = frame_field ? to_plaintext(frame_field.children) : "none";
@@ -32,14 +32,7 @@ export function CardEditor(props: CardEditorProps) {
 				<TextField card_path={path} field={"type"} min_font_size={5} max_font_size={13}/>
 
 				<div className="set_symbol"></div>
-			</div> 
-			{/* <div className="text_box">
-				<TextField card_path={path} field={"rules_text"}/>
-				<div className="visual_box"></div>
-				<hr className="flavor_bar"/>
-				<TextField card_path={path} field={"flavor_text"}/>
-				<div className="visual_box"></div>
-			</div> */}
+			</div>
 			<TextField card_path={path} field={"card_text"} min_font_size={6} max_font_size={14}/>
 			<PowerToughnessBackground card_path={path} field={"pt"}/>
 			<TextField card_path={path} field={"pt"} min_font_size={8} max_font_size={16}/>
